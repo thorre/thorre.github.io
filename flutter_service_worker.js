0254213a2dfb4980f5baec1c36bafb9eb01073be
Jonathan Thompson <jonathan.thompson@ica.se>
2020-03-30 13:00:23 +0200

    Added clock

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,15 +2,16 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "1bce9673fff823c25b7a319a64f04bc1",
-"/main.dart.js": "b5358ee903f2a38a2857581791aaaac3",
-"/favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/main.dart.js": "f668361596621bde99ff150341e872a9",
+"/favicon.png": "ad740e900e2d1cc15babcc88c1fb84a8",
+"/icons/Icon-192.png": "9f94c1998b1ff2194f11a65787c889f8",
+"/icons/Icon-512.png": "a70399c3892a2231bb06141e22e8a0c3",
 "/manifest.json": "66778a44a1e8003718d1c3f1d4e4550c",
 "/assets/LICENSE": "441127c477dc8ff46f4115c86a7ee6ed",
-"/assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
-"/assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+"/assets/AssetManifest.json": "091f4130822431d02524c9685e8abadb",
+"/assets/FontManifest.json": "772c3bc37b3ab152157969e0f111c4f6",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/fonts/CourierPrime-Regular.ttf": "03ef685b34f3356631c3f60c91917002"
 };
 
 self.addEventListener('activate', function (event) {
